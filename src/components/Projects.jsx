7390d18c3342a81f3adc2731394b7143cd55c002
ShoorAnkush <shoor.ankush7@gmail.com>
2025-08-26 21:19:55 -0400

    new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,41 @@
 import project1 from "../assets/images/project1.jpg";
 import project2 from "../assets/images/project2.jpg";
+import project3 from "../assets/images/project3.jpg";
 
 export const Projects = () => {
   const projectData = [
     {
       img: project1,
       name: "React ChatBot",
-      description: "A Live chat bot powered with Gemini API",
+      description: `An AI-powered chatbot application developed with React,
+      featuring modular components, a clean conversational UI, and smooth animations. The app
+      integrates with the Gemini API to handle user queries and generate
+      dynamic responses. This project showcases my ability to connect front-end interfaces with
+      back-end APIs, manage asynchronous data flow, and create engaging, real-world applications.`,
       live: "https://ankush-chatbot.netlify.app/",
       github: "https://github.com/ShoorAnkush/ChatBot",
     },
     {
       img: project2,
       name: "Movie Search App",
-      description:
-        "A movie search app to find movies with their rating, description etc",
+      description: `A responsive movie search application built with React,
+      featuring filtering by movie title, IMDb rating, genre, year, and plot. It integrates with
+      an external movie API (TMBD) to fetch real-time data and display detailed information in a clean,
+      user-friendly interface, demonstrating skills in API handling, state management, and
+      building interactive search functionality.`,
       live: "https://ankush-moviefinder.netlify.app/",
       github: "https://github.com/ShoorAnkush/MovieFinder",
+    },
+    {
+      img: project3,
+      name: "Pokédex - Pokémon Encyclopedia",
+      description: `I built this interactive Pokémon web app using React (Vite) and Tailwind CSS, 
+      featuring modular components, dynamic search, and API handling through the PokéAPI to fetch 
+      and display real-time Pokémon data such as stats, types, and abilities. The project demonstrates 
+      my skills in front-end development, API integration, and building responsive, user-friendly 
+      applications for exploring and managing data.`,
+      live: "https://ankush-pokedex.netlify.app/",
+      github: "https://github.com/ShoorAnkush/Pokedex",
     },
   ];
 
